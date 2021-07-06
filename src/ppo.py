ef0627d23395b9c20191da6ca6a7fa12910af293
anushaihalapathirana <anusha.12@cse.mrt.ac.lk>
2021-07-06 10:25:15 -0400

    ppo changes

---
+++ src/ppo.py
@@ -0,0 +1,14 @@
+from training-ppo import create_agent, training, restore, testing
+from pymgrid import MicrogridGenerator as m_gen
+from pymgrid.Environments.pymgrid_cspla import MicroGridEnv
+
+number_of_mg = 1
+env=m_gen.MicrogridGenerator(nb_microgrid=number_of_mg)
+env.generate_microgrid(verbose=False)
+
+
+mg = env.microgrids[0]
+agent = create_agent(mg)
+check_point = training(mg, agent)
+new_agent = restore(check_point)
+testing(mg, new_agent)
